ce3806b49f0e021402e7d1eb54aee6e6af55aa0c
Semyon Moskvartsev <semyon.moskvartsev@arcadia.spb.ru>
2021-07-05 15:01:00 +0300

    Change dirname of web-server

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + "/dist"));
+
 
 app.listen(PORT, () => {
   console.log(`Server runs on http://localhost:${PORT}`)
-  console.log(`dirname ${__dirname}`)
 })
--- src/modules/form.module.js
+++
@@ -1,7 +0,0 @@
-export function getInput() {
-
-}
-
-export function getForm() {
-
-}
