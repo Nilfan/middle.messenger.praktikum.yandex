909efde42016fab3afab32587dc3da647f727599
Semyon Moskvartsev <semyon.moskvartsev@arcadia.spb.ru>
2021-07-05 16:24:54 +0300

    Fix server

--- server.js
+++ server.js
@@ -6,11 +6,6 @@ const PORT = 3000;
 
 app.use(express.static(__dirname + "/dist"));
 
-
-app.get("*", function (req, res) {
-  res.sendFile(pathUtils.resolve(__dirname + "/dist" + "/index.html"));
-});
-
 app.listen(PORT, () => {
   console.log(`Server runs on http://localhost:${PORT}`)
 })
