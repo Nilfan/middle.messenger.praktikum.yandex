df277526f231aff698cc750390ea38649e8d76aa
Semyon Moskvartsev <semyon.moskvartsev@arcadia.spb.ru>
2021-08-29 20:47:32 +0300

    Fix webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const production = (process.env.WEBPACK_ENV || process.env.NODE_ENV) === 'production'
 
